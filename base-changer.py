5f7e72a2996a651ef213c24387a2bf224f98a63f
belugaman9981 <littledage@outlook.com>
2026-03-15 21:01:49 -0700

    Update base-changer.py

--- base-changer.py
+++ base-changer.py
@@ -2,5 +2,7 @@
 Number   = int(input("What is the number you want to transform? "))
 intoBase = int(input("What base do you want to convert to? "))
 
-for i in range():
-    
+i = 0
+
+while intoBase != 0:
+    Number // intoBase
