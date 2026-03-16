5bc633ddbcf6ea1a457f6e6995d8d2108375c728
belugaman9981 <littledage@outlook.com>
2026-03-16 10:50:24 -0700

    Update base-changer.py

--- base-changer.py
+++ base-changer.py
@@ -4,10 +4,12 @@ Base = int(input("What base do you want to convert to? "))
 
 list = []
 i = 0
+StringNum = 0
 
 while Number != 0:
     Remainder = Number % Base
     Number = Number // Base
     list.append(Remainder)
+    StringNum =+ list.pop()
     
 print(list)
