1bca472266505f7ff994aae1f5fe47725ef70855
belugaman9981 <littledage@outlook.com>
2026-03-17 22:01:48 -0700

    Update base-changer-test.py

--- base-changer-test.py
+++ base-changer-test.py
@@ -1,11 +1,14 @@
-number = input("Enter the number: ")
-base = int(input("Enter the base of the you want to tranform number into: "))
-    
+number = input("Enter the number: ").upper()
+base = int(input("Enter the base of the number: "))
+
 decimal = 0
 power = 0
 
-for digit in number:
-    decimal += int(digit) * (base ** power)
+digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for digit in reversed(number):
+    value = digits.index(digit)
+    decimal += value * (base ** power)
     power += 1
-        
-print("Base", base, "value is:", decimal)
+
+print("Base 10 value is:", decimal)
