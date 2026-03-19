0ef78044086a010c3606c49fcae01ab8744a87fd
belugaman9981 <littledage@outlook.com>
2026-03-18 22:18:45 -0700

    Update base-changer-scam.py

--- base-changer-scam.py
+++ base-changer-scam.py
@@ -1,13 +1,24 @@
 number = int(input("What is the number you want to transform? "))
 
+s = 0
+
 def doNothing(self):
-    for i in range(self)
+    global s
+    
+    for i in range(self):
+        i += 1
 
 i = 100
 
 while i > 0:
     for i in range():
         if i > 0 == True:
+            i -= 1
+            
+        else:
+            exit()
+            
+    for x in 
             
 
 print(number)
